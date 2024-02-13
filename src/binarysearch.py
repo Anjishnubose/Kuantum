358f6ddc82f0d90eb6728ae1a3ac377742384b76
unknown <Serene>
2024-02-13 12:35:08 -0500

    oops forgot to save the first time

--- src/binarysearch.py
+++ src/binarysearch.py
@@ -3,9 +3,9 @@ from pennylane import numpy as np
 from typing import Callable
 
 #certify subroutine, returns 0 if C(x+delta) > eta/2, 1 if C(x-delta) < eta, random if both
-def certify(x: float, delta: float, eta: float, acdf: Callable):
+def certify(x: float, delta: float, eta: float, acdf: Callable) -> int:
     if acdf(x) > 5*eta/8:
-        if acdf(x - delta) < 7*eta/8:
+        if acdf(x) < 7*eta/8:
             cert_output = np.random.randint(2)
         else:
             cert_output = 0
@@ -13,14 +13,14 @@ def certify(x: float, delta: float, eta: float, acdf: Callable):
         cert_output = 1
     return cert_output
 
-def invert_cdf(delta: float, eta: float, acdf: Callable):
+def invert_cdf(delta: float, eta: float, acdf: Callable, certify: Callable) -> float:
     #initialize x_0 and x_1 values
     x_0, x_1 = -np.pi/3, np.pi/3
     while x_1 - x_0 > 2*delta:
-        x = (x_0 + x_1)/2
-        u = certify(x, 2*delta/3, eta, acdf)
-        if u ==0:
-            x_1 = x + 2*delta/3
+        x = (x_0 + x_1)/2 #set x to be midpoint between x_0 and x_1
+        u = certify(x, 2*delta/3, eta, acdf) #use certify subroutine to check if C(x+(2/3)delta) > eta/2 or C(x-(2/3)delta) < eta
+        if u == 0:
+            x_1 = x + 2*delta/3 #move new x_1 point closer to x if C(x+delta) > eta/2
         else:
             x_0 = x - 2*delta/3
     return (x_0 + x_1)/2
