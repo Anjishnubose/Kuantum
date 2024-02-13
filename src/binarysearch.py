8e0bb012dcedc0448dd2e0291ce9775292b7d722
asymptoticgains <Serene>
2024-02-13 17:18:35 -0500

    Fixed comment

--- src/binarysearch.py
+++ src/binarysearch.py
@@ -15,9 +15,9 @@ def invert_cdf(delta: float, eta: float, acdf: Callable) -> float:
     x_0, x_1 = -np.pi/2, np.pi/2
     while x_1 - x_0 > 2*delta:
         x = (x_0 + x_1)/2 #set x to be midpoint between x_0 and x_1
-        u = certify(x, 2*delta/3, eta, acdf) #use certify subroutine to check if C(x+(2/3)delta) > eta/2 or C(x-(2/3)delta) < eta
+        u = certify(x, 2*delta/3, eta, acdf) #use certify subroutine to check if C(x+(2/3)delta) > 0 or C(x-(2/3)delta) < eta
         if u == 0:
-            x_1 = x + 2*delta/3 #move new x_1 point closer to x if C(x+(2/3)delta) > eta/2
+            x_1 = x + 2*delta/3 #move new x_1 point closer to x if C(x+(2/3)delta) > 0
         else:
             x_0 = x - 2*delta/3 #move new x_0 point closer to x if C(x-(2/3)delta) < eta
     return (x_0 + x_1)/2
