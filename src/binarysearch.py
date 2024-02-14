671078ba77335724c3a0d9c9a706da8be161707c
asymptoticgains <serene.shum@mail.utoronto.ca>
2024-02-14 17:07:48 -0500

    Fixed minor errors

--- src/binarysearch.py
+++ src/binarysearch.py
@@ -2,7 +2,6 @@ import scipy as sp
 from pennylane import numpy as np
 from typing import Callable
 
-
 def acdf(real_g: np.array, imag_g: np.array, sampled_index: np.array, S: float) -> Callable:
     """
     importance-sampled approximate cumulative distribution function built from samples
@@ -17,11 +16,12 @@ def acdf(real_g: np.array, imag_g: np.array, sampled_index: np.array, S: float) 
         return g
     return H
 
-def certify_mv(x: float, real_g: np.array, imag_g: np.array, sampled_index: np.array, eta: float, S: float, N_B: int, M: int) -> float:
+def certify_mv(x: float, real_g: np.array, imag_g: np.array, sampled_index: np.array, eta: float, S: float, N_B: int, M: int) -> int:
     """
     certify function by majority vote procedure in LT2022
     checks whether C(x - delta) > eta/2 or C(x - delta) < eta using sampled approximate CDF
-    N_B is number of batches in majority vote procedure, M is the total number of samples taken from the circuits"""
+    N_B is number of batches in majority vote procedure, M is the total number of samples taken from the circuits
+    """
     cert_output = 0
     #initialize counter in majority vote
     c = 0
@@ -58,8 +58,10 @@ def certify_av(x: float, real_g: np.array, imag_g: np.array, sampled_index: np.a
         cert_output = 1
     return cert_output
 
-#invert CDF using either average certify subroutine or majority vote certify subroutine, default is average
-def invert_cdf(real_g: np.array, imag_g: np.array, sampled_index: np.array, delta: float, eta: float, M: int, S: float, certify_type: str = "average", x_0: float = -np.pi/3, x_1: float = np.pi/3, N_B: int = 0, g: Callable = acdf) -> float: 
+def invert_cdf(real_g: np.array, imag_g: np.array, sampled_index: np.array, delta: float, eta: float, M: int, S: float, certify_type: str = "average", x_0: float = -np.pi/2, x_1: float = np.pi/2, N_B: int = 0, g: Callable = acdf) -> float: 
+    """
+    invert CDF using either average certify subroutine or majority vote certify subroutine (default is average)
+    """
     while x_1 - x_0 > 2*delta:
         x = (x_0 + x_1)/2 #set x to be midpoint between x_0 and x_1
         #use certify subroutine to check if C(x+(2/3)delta) > 0 or C(x-(2/3)delta) < eta
@@ -72,6 +74,3 @@ def invert_cdf(real_g: np.array, imag_g: np.array, sampled_index: np.array, delt
         else:
             x_0 = x - 2*delta/3 #move new x_0 point closer to x if C(x-(2/3)delta) < eta
     return (x_0 + x_1)/2
-
-
-    
