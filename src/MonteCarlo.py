a4f5efc9156934441b4b5c1ada492f0cbc6fa7d9
Anjishnu Bose <39477277+Anjishnubose@users.noreply.github.com>
2024-02-14 20:11:30 -0500

    added thermalization to monte carlo sampling

--- src/MonteCarlo.py
+++ src/MonteCarlo.py
@@ -5,12 +5,13 @@ from pennylane import numpy as np
 """
 function to run a simple Monte Carlo simulation sampling the heaviside function F(k; beta).
 args:
+    N_thermalization: int - number of steps to thermalize the system.
     N_sample: int - number of samples to take
     N_shot: int - number of shots per sample
     k_max: int - maximum value of k
     beta: float - parameter of the heaviside function
 """
-def Metropolis(N_sample: int, k_max: int, 
+def Metropolis(N_thermalization: int, N_sample: int, k_max: int, 
                 beta: float, reduced_range:int = 51, move_range:int = 101) -> dict:
     """
     This function uses the Metropolis algorithm to sample from the periodic Heaviside function.
@@ -25,13 +26,12 @@ def Metropolis(N_sample: int, k_max: int,
     reduced_j_range = (0, reduced_range)
     j_current = np.random.randint(*reduced_j_range)
     k_current = 2*j_current+1
-    sample[int(j_current)] += 1
-    probabilities[int(k_current)] = hv.heaviside_fourier_reduced(k_current, k_max, beta)
+    probabilities[int(k_current)] = hv.heaviside_fourier_reduced_mp(k_current, k_max, beta)
     ##### initialize the number of accepted states
     accepted = 0
     ##### run the Metropolis algorithm
     print(f"Running Metropolis algorithm for {N_sample} samples starting at {k_current}.")
-    for step in range(N_sample):
+    for step in range(N_thermalization+N_sample):
         if step%1000 == 0:
             print(f"Step {step} of {N_sample}.")
         ##### propose a new state within move_range of the current state.
@@ -39,14 +39,16 @@ def Metropolis(N_sample: int, k_max: int,
         k_new = int(2*j_new+1)
         ##### calculate the acceptance probability
         if k_new not in probabilities:
-            probabilities[int(k_new)] = hv.heaviside_fourier_reduced(k_new, k_max, beta)
+            probabilities[int(k_new)] = hv.heaviside_fourier_reduced_mp(k_new, k_max, beta)
         
         p_accept = probabilities[int(k_new)] / probabilities[int(k_current)]
         p_accept = min(1, p_accept)
         ##### accept or reject the new state randomly
         if np.random.rand() < p_accept:
             ##### updating the sample count of the new state
-            sample[int(j_new)] += 1   
+            if step >= N_thermalization:
+                sample[int(j_new)] += 1
+                
             j_current = np.copy(j_new)
             k_current = np.copy(k_new)
             accepted += 1
@@ -61,10 +63,11 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     
     N_sample = 10_000
+    N_thermalization = 10_000
     k_max = 1_001
     beta = 1E6
     
-    sample = Metropolis(N_sample, k_max, beta)
+    sample = Metropolis(N_thermalization, N_sample, k_max, beta)
     
     plt.plot(range(1, k_max+2, 2), sample/N_sample, label = r'$\beta = {:.2e}, k_{{max}} = {:d}.$'.format(beta, k_max))
     plt.legend()
