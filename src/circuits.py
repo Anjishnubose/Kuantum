18a971a044006a6eba8aa6ff814f1045255a92f5
Praveen Jayakumar <praveen.jayakumar@mail.utoronto.ca>
2024-02-15 00:19:36 -0500

    Modified circuit contruction

--- src/circuits.py
+++ src/circuits.py
@@ -11,7 +11,22 @@ def get_U(H, tau, k):
     coeff = (-1.j)*tau*k
     return qml.exp(hamiltonian, coeff).matrix()
 
-def hadamard_test(U, hf_list, n_qubits, measure='real', control_wires=[0]):
+def prepare_hf_gs(hf_list, start_wire = 0):
+    targets = [i for i, a in enumerate(hf_list) if a == 1]
+    for wire in targets:
+        qml.PauliX(wires=wire + start_wire)
+
+def prepare_initial_state(state_type = 'hf', start_wire=1, **kwargs):
+    """
+    prepare initial state
+
+    """
+    if state_type == 'hf':
+        hf_list = kwargs['hf_list']
+        prepare_hf_gs(hf_list=hf_list, start_wire=start_wire)
+    return
+
+def hadamard_test(U, n_qubits, measure='real', control_wires=[0]):
     """
     Returns Hadamard test samples
     
@@ -19,9 +34,6 @@ def hadamard_test(U, hf_list, n_qubits, measure='real', control_wires=[0]):
     
     #hamiltonian_matrix = qml.matrix(H)
     wires = [1 + a for a in range(n_qubits)] #target
-
-    #adds X gates to initialize psi
-    prepare_hf_gs(hf_list=hf_list, start_wire=len(control_wires))
     
     qml.Hadamard(wires=control_wires)
     qml.ControlledQubitUnitary(U, control_wires=control_wires, wires=wires)
@@ -30,13 +42,6 @@ def hadamard_test(U, hf_list, n_qubits, measure='real', control_wires=[0]):
     if measure=='imag':
         qml.adjoint(qml.S(wires=control_wires))
     qml.Hadamard(wires=control_wires)
-
-    return qml.sample(wires=[0])
-
-def prepare_hf_gs(hf_list, start_wire = 0):
-    targets = [i for i, a in enumerate(hf_list) if a == 1]
-    for wire in targets:
-        qml.PauliX(wires=wire + start_wire)
 
 # import matplotlib.pyplot as plt
 def exp_from_samples(samples):
@@ -53,9 +58,19 @@ def get_tau(hamiltonian, norm_bound = np.pi/2):
     """
     return norm_bound/np.linalg.norm(qml.matrix(hamiltonian))
 
+def make_circuit(U, n_qubits, hf_list, measure='real'):
+
+    control_wires = [0]
+    #adds X gates to initialize psi
+    prepare_initial_state(state_type='hf', hf_list=hf_list, start_wire=len(control_wires))
+    hadamard_test(U, n_qubits=n_qubits, measure=measure, control_wires=control_wires)
+
+    return qml.sample(wires=[0])
+
+
 def get_gk(k, nk, hamiltonian, n_qubits, hf_list, tau, measure = 'real'):
     """
-    Run hadamard test and get samples and ancilla expectation.
+    Run state prep, hadamard test and get samples and ancilla expectation.
 
     measure: 'real' / 'imag'
 
@@ -65,11 +80,11 @@ def get_gk(k, nk, hamiltonian, n_qubits, hf_list, tau, measure = 'real'):
     """
     
     dev = qml.device("default.qubit", wires=n_qubits+1, shots=nk)
-    full_qnode = qml.QNode(hadamard_test,dev)
+    circuit_qnode = qml.QNode(make_circuit,dev)
 
     U = get_U(H=hamiltonian, tau=tau, k=k)
 
-    samples = full_qnode(U, hf_list = hf_list, n_qubits=n_qubits, measure=measure)
+    samples = circuit_qnode(U, n_qubits=n_qubits, hf_list = hf_list, measure=measure)
     return samples, exp_from_samples(samples)
 
 def get_rs_lists(nk_list, hamiltonian, n_qubits, hf_list, tau):
