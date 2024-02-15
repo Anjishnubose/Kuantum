31f54584441eee89d4229375943f1c85a570c9db
Praveen Jayakumar <praveen.jayakumar@mail.utoronto.ca>
2024-02-14 21:13:44 -0500

    Added main function and sample code

--- tests/samplecode.py
+++ tests/samplecode.py
@@ -1,8 +1,9 @@
-from Kuantum.src.circuits import get_gk, get_tau
-from Kuantum.src.hamiltonians import get_hamiltonian
-
 import pennylane as qml
 import pennylane.numpy as np
 
+from Kuantum.src.circuits import get_gk, get_tau, get_rs_lists
+from Kuantum.src.hamiltonians import get_hamiltonian
+
 H, n_qubits, hf_list = get_hamiltonian('h2')
-get_gk(0, 1000, H, hf_list=[1, 1, 0, 0], n_qubits=n_qubits, tau = get_tau(H), measure='imag')
+
+get_rs_lists([2, 2, 2, 1, 0, 10], hamiltonian=H, n_qubits=n_qubits, hf_list=hf_list, tau = get_tau(H))
