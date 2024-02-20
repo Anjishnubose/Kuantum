3a91ffccad617c8a05a0610cd4ef90b48aefb933
asymptoticgains <serene.shum@mail.utoronto.ca>
2024-02-20 16:40:45 -0500

    :(

--- src/samplecode.py
+++ src/samplecode.py
@@ -3,11 +3,11 @@ import pennylane.numpy as np
 import matplotlib.pyplot as plt
 import yaml as yml
 
-input_file = 'C:/Users/anjis/Documents/Kuantum/inputs/input.yaml'
+input_file = 'C:/Users/sstb2/Kuantum/inputs/input.yaml'
 with open(input_file, 'r') as file:
     inputs = yml.safe_load(file)
 
 print(inputs)
 
 import PhaseEstimation as pe
-output  = pe.StatisticalPhaseEstimation(inputs)
+output  = pe.StatisticalPhaseEstimation(inputs, to_plot = True, plot_path="C:/Users/sstb2/Kuantum/figures/plot.png")
