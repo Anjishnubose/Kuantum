a2d29d7de92fd493b68a9e0928abf18b6eb05c1a
Anjishnu Bose <39477277+Anjishnubose@users.noreply.github.com>
2024-02-22 12:26:27 -0500

    testing stuff

--- src/samplecode.py
+++ src/samplecode.py
@@ -12,5 +12,5 @@ print(inputs)
 
 import PhaseEstimation as pe
 output  = pe.StatisticalPhaseEstimation_wtLCU(inputs, 
-                                                to_plot=False, plot_path='C:/Users/anjis/Documents/Kuantum/figures/plot_H2_LCU.png',
+                                                to_plot=True, plot_path='C:/Users/anjis/Documents/Kuantum/figures/plot_H2_2.png',
                                                 to_save=False, save_path='C:/Users/anjis/Documents/Kuantum/outputs/output_H2_LCU.pkl')
